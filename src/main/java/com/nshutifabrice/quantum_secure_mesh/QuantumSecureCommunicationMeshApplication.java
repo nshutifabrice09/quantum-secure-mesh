d25c67a0359731a2afdee68ea2ecd45fad4bc8f5
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-10-25 10:22:37 +0200

    first commit

---
+++ src/main/java/com/nshutifabrice/quantum_secure_mesh/QuantumSecureCommunicationMeshApplication.java
@@ -0,0 +1,13 @@
+package com.nshutifabrice.quantum_secure_mesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantumSecureCommunicationMeshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuantumSecureCommunicationMeshApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nshutifabrice/quantum_secure_mesh/QuantumSecureCommunicationMeshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nshutifabrice.quantum_secure_mesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuantumSecureCommunicationMeshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
